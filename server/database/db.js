07ae48ebef178d037c99e1b60b3d7144389343ee
dahiisworking <157959209+vaidehiparashar@users.noreply.github.com>
2026-05-29 10:08:49 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { createClient } = require('@libsql/client');
 
 const db = createClient({
-  url: process.env.TURSO_URL,
+  url: 'libsql://hrms-db-parasharvaidehi2.aws-ap-south-1.turso.io',
   authToken: process.env.TURSO_TOKEN
 });
 
